255ae5c3b46a016e42e8fcd47a806485f54e3380
kimgyuha <kimgyuha25@gmail.com>
2025-01-12 01:40:22 +0900

    feat :: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import 'swiper/css';
+import "swiper/css";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 
