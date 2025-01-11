ccf73c98c711dbb6267aceb72d148ea6b9611905
eternrust <overload829@gmail.com>
2025-01-12 04:56:46 +0900

    feat :: chat list

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MatchDetail from "./pages/MatchDetail";
 import Match from "./pages/Match";
 import Chat from "./pages/Chat";
 import Community from "./pages/Community";
+import ChatList from "./pages/ChatList";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/match" element={<Match />} />
         <Route path="/match/:id" element={<MatchDetail />} />
+        <Route path="/chat" element={<ChatList />} />
         <Route path="/chat/:id" element={<Chat />} />
         <Route path="/community" element={<Community />} />
         <Route path="/profile" element={<>profile</>} />
